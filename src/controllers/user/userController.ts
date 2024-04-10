5f0cfb6b862ef9a053bb53be90cb7e576fa8ff50
Haider <haideraliamjad4391@gmail.com>
2024-04-10 12:56:17 -0700

    improving error hadnling

--- src/controllers/user/userController.ts
+++ src/controllers/user/userController.ts
@@ -11,24 +11,29 @@ const registerUser = async (
   res: Response,
   next: NextFunction
 ) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
+  const user = await userModel.findOne({ email: email });
+  if (user) {
+    const error = createHttpError(400, "user already register");
+    return next(error);
+  }
+  let newUser = null;
   try {
-    const { name, email, password } = req.body;
-    if (!name || !email || !password) {
-      const error = createHttpError(400, "All fields are required");
-      return next(error);
-    }
-    const user = await userModel.findOne({ email: email });
-    if (user) {
-      const error = createHttpError(400, "user already register");
-      next(error);
-    }
     const hashPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await userModel.create({
+    newUser = await userModel.create({
       name,
       password: hashPassword,
       email,
     });
+  } catch (error) {
+    return next(createHttpError(500, "Error while registering user"));
+  }
+  try {
     const token = sign(
       {
         sub: newUser._id,
@@ -40,7 +45,7 @@ const registerUser = async (
     );
     return res.json({ id: newUser._id, token });
   } catch (error) {
-    return next(error);
+    return next(createHttpError(500, "error while generating access token"));
   }
 };
 
