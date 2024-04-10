8121b225a1b144d7f01ad804e6f13ec68939c6fa
Haider <haideraliamjad4391@gmail.com>
2024-04-10 12:38:25 -0700

    sign jwt token

--- src/routers/user/userRouter.ts
+++ src/routers/user/userRouter.ts
@@ -1,7 +1,5 @@
-import { Router } from "express"
-import { registerUser } from "../../controllers/user/userController"
-const userRouter = Router()
-
-userRouter.post('/users/register',registerUser)
-
-export {userRouter}
+import { Router } from "express";
+import { registerUser } from "../../controllers/user/userController";
+const userRouter = Router();
+userRouter.post("/users/register", registerUser);
+export { userRouter };
